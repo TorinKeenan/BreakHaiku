c1ef6a28a4d87f7231af3921b0a334e7e11a9fe1
Torin Keenan <tkeenanbusiness@gmail.com>
2018-11-06 23:14:45 -0800

    readable code

--- server.js
+++ server.js
@@ -23,50 +23,38 @@ class Haiku{
 		this.line_3 = line_3;
 	}
 }
-function check_haiku(haiku){
-	//checks for 5/7/5 and reference to bread
-	return (syllable(haiku.line_1) == 5 && syllable(haiku.line_2) == 7 && syllable(haiku.line_3) == 5 ) && 
-	(haiku.line_1.tolowercase().includes('bread') || haiku.line_2.tolowercase().includes('bread') || 
-		haiku.line_3.tolowercase().includes('bread'));
-};
+
 
 app.get('/',function (req,res) {
 	res.send('Homepage');
 });
 
 app.get('/haikus',function (req,res) {
+	con.connect(function(err) {
+	if (err) throw err;
+    console.log("Connected!");
 		con.connect(function(err) {
-	  if (err) throw err;
-	  console.log("Connected!");
-	 con.connect(function(err) {
-	  if (err) console.error(err.message);
-	  
-	  console.log("Connected!");
-	  con.query(`SELECT author,line_1,line_2,line_3 FROM HAIKUS ORDER BY id DESC`, function (err, result) {
-	    if (err) throw err;
-	    return(result);
-	  });
-	});
+			if (err) console.error(err.message);
+		 	console.log("Connected!");
+		    con.query(`SELECT author,line_1,line_2,line_3 FROM HAIKUS ORDER BY id DESC`, function (err, result) {
+			    if (err) throw err;
+			    return(result);
+		    });
+		});
 	});
 });
 
 app.post('/add_haiku',function(req,req){
 	con.connect(function(err) {
-	console.log(req.body)
-	for (haiku in req.body){
-		new_haiku = Haiku()
-	
-			
-			  if (err) throw err;
-			  console.log("Connected!");
+		console.log(req.body)
+		for (haiku in req.body){			
+			if (err) throw err;
+			    console.log("Connected!");
 				con.query(`Insert INTO haikus (author,line_1,line_2,line_3) VALUES ?`,
 					[haiku.author,haiku.line_1,haiku.line_2,haiku_1.line_3], function (err, result) {
-			    if (err) res.status(500).json({ error: err.message });;
-			    
-			  
-	});
-		
-	}
+				    	if (err) res.status(500).json({ error: err.message });;
+					});
+				}
 	}).then(function(){
 		res.send(200)
 	});
